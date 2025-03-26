3255ade58ba6b2cd9868d1a64e03d2de811c114a
LeThanh0023 <thanhle0023@gmail.com>
2025-03-26 12:48:58 +0700

    fix

--- StudentTeacherUI_Tuan2/app/src/main/java/com/example/studentteacherui/MainActivity.java
+++ StudentTeacherUI_Tuan2/app/src/main/java/com/example/studentteacherui/MainActivity.java
@@ -8,12 +8,16 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.annotation.Nullable;
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_CODE = 99;
     private EditText editTextStudent, editTextCorrected;
-    private Button buttonSubmit;
+    private Button buttonSubmit, buttonClear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +27,21 @@ public class MainActivity extends AppCompatActivity {
         editTextStudent = findViewById(R.id.editTextStudent);
         editTextCorrected = findViewById(R.id.editTextCorrected);
         buttonSubmit = findViewById(R.id.buttonSubmit);
+        buttonClear = findViewById(R.id.buttonClear);
 
         buttonSubmit.setOnClickListener(v -> {
             String studentText = editTextStudent.getText().toString();
             Intent intent = new Intent(MainActivity.this, SecondActivity.class);
             intent.putExtra("student_text", studentText);
             startActivityForResult(intent, REQUEST_CODE);
+        });
+        buttonClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editTextStudent.setText("");
+                editTextCorrected.setText("");
+
+            }
         });
     }
 
@@ -41,4 +54,6 @@ public class MainActivity extends AppCompatActivity {
             editTextCorrected.setText(correctedText);
         }
     }
+
+
 }
